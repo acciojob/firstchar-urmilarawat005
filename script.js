0404d3b2015df8509def0f8e026fba81ff3e02d4
urmilarawat005 <u4urmilarawat@gmail.com>
2025-01-16 20:12:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 function firstChar(text) {
-
-	let stripped = text.trimStart();
-	if(stripped){
-		return stripped[0];
-	}else{
-		return '';
-	}
-  // your code here
+    // Trim leading spaces and check if the string is not empty
+    let stripped = text.trimStart(); // Removes leading spaces
+    if (stripped.length > 0) {
+        return stripped[0]; // Return the first non-space character
+    } else {
+        return ''; // Return an empty string if there are no non-space characters
+    }
 }
-let text = " Rosa Parks ";
 
 // Do not change the code below
-//Uncomment the following line to show the prompt popup
-//const text = prompt("Enter text:");
-alert(firstChar(text));
+// Uncomment the following line to show the prompt popup
+const text = prompt("Enter text:"); // Get user input using prompt
+alert(firstChar(text)); // Alert the result of the firstChar function
+
